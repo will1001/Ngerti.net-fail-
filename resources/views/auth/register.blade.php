f186f8ea1eb99f72961e91f440f33f6440660ecb
will1001 <wilirahmatm@gmail.com>
2019-03-19 11:24:28 +0800

    sebagai added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,21 @@
                             </div>
                         </div>
 
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Sebagai') }}</label>
+
+                            <div class="col-md-6">
+                                
+
+                                <select name="sebagai" id="sebagai" class="form-control">
+                                    <option value="Teacher">Teacher</option>
+                                    <option value="Student">Student</option>
+                                </select>
+
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
